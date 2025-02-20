86d7abaf2191169998c6aea25189fbb647b6c0d8
GameKnave <git@gameknave.com>
2025-02-20 14:10:38 -0800

    feature/ionic capacitor config

---
+++ client/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.angularmomentum',
+  appName: 'Angular Momentum',
+  webDir: 'dist/angular-momentum'
+};
+
+export default config;
