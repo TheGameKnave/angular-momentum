2270d55cd81f17222e9dbf5cfa85ba53f33f6b4d
GameKnave <git@gameknave.com>
2025-02-20 14:24:07 -0800

    feature/ionic capacitor/appflow config fixes

--- client/capacitor.config.ts
+++ client/capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'app.angularmomentum',
   appName: 'Angular Momentum',
-  webDir: 'dist/angular-momentum'
+  webDir: 'dist/angular-momentum/browser'
 };
 
 export default config;
