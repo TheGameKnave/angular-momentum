23a4cca2ca16b949efbca59a2400307396cb3f2f
GameKnave <git@gameknave.com>
2025-07-25 23:49:16 -0700

    [socket.io] environment file fixes (again)

--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,10 +1,4 @@
-// Function to get the base URL from the current location
-function getBaseUrl(): string {
-  const { protocol, hostname, port } = window.location;
-  return `${protocol}//${hostname}${port ? ':' + port : ''}`;
-}
-
 export const ENVIRONMENT = {
-  baseUrl: getBaseUrl(),
+  baseUrl: window.location.origin,
   env: 'development',
 };
