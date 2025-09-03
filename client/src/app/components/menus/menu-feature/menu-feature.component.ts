d285571f426e536b5fd7e22f5caf86e911b2ae3f
GameKnave <git@gameknave.com>
2025-09-03 15:58:08 -0700

    primeng dynamically moving menu on navigation

--- client/src/app/components/menus/menu-feature/menu-feature.component.ts
+++ client/src/app/components/menus/menu-feature/menu-feature.component.ts
@@ -1,11 +1,23 @@
-import { ChangeDetectionStrategy, Component, HostListener, signal } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  HostListener,
+  signal,
+  ElementRef,
+  AfterViewInit,
+  NgZone,
+  DestroyRef
+} from '@angular/core';
+import { Router, NavigationEnd, RouterModule } from '@angular/router';
+import { filter } from 'rxjs';
 import { TranslocoDirective } from '@jsverse/transloco';
 import { SlugPipe } from '@app/pipes/slug.pipe';
 import { ComponentListService } from '@app/services/component-list.service';
 import { FeatureFlagService } from '@app/services/feature-flag.service';
 import { HelpersService } from '@app/services/helpers.service';
 import { SCREEN_SIZES } from '@app/helpers/constants';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { TooltipModule } from 'primeng/tooltip';
 
 @Component({
   selector: 'app-menu-feature',
@@ -15,34 +27,79 @@ import { SCREEN_SIZES } from '@app/helpers/constants';
     SlugPipe,
     RouterModule,
     TranslocoDirective,
+    TooltipModule,
   ],
-  styleUrls: ['./menu-feature.component.scss'],
 })
-export class MenuFeatureComponent {
-  expanded = signal(false);
-  constructor(
-    readonly componentListService: ComponentListService,
-    protected featureFlagService: FeatureFlagService,
-    protected readonly helpersService: HelpersService,
-  ) {}
-
-  componentCount(): number {
-    return this.helpersService.enabledComponents().length;
-  }
-  // Detect mouseEnter and expand
+export class MenuFeatureComponent implements AfterViewInit {
+  // Mouse hover for desktop
   @HostListener('mouseenter')
   onMouseEnter() {
-    if(window.innerWidth >= SCREEN_SIZES.md) this.expanded.set(true);
+    if (window.innerWidth >= SCREEN_SIZES.md) this.expanded.set(true);
   }
   @HostListener('mouseleave')
   onMouseLeave() {
     this.expanded.set(false);
   }
-  // close the menu on click
   @HostListener('click')
   onClick() {
     this.expanded.set(false);
   }
-  
-  
+  @HostListener('window:resize')
+  onResize() {
+    if (!this.host.nativeElement) return;
+    this.containerCenter = this.host.nativeElement.clientWidth / 2;
+    // trigger template update in a cheaty way
+    this.expanded.update(v => v);
+  }
+  expanded = signal(false);
+
+  private containerCenter = 0;
+
+  constructor(
+    readonly componentListService: ComponentListService,
+    protected featureFlagService: FeatureFlagService,
+    protected readonly helpersService: HelpersService,
+    private readonly router: Router,
+    private readonly ngZone: NgZone,
+    private readonly destroyRef: DestroyRef,
+    private readonly host: ElementRef,
+  ) {}
+
+  ngAfterViewInit() {
+    this.scrollToCenter();
+    // Scroll to active route on mobile only
+    this.router.events
+      .pipe(
+        filter(e => e instanceof NavigationEnd),
+        takeUntilDestroyed(this.destroyRef)
+      )
+      .subscribe(() => {
+        this.scrollToCenter();
+      });
+  }
+
+  scrollToCenter(): void {
+    if (window.innerWidth < SCREEN_SIZES.md) {
+      this.ngZone.runOutsideAngular(() => {
+        requestAnimationFrame(() => {
+          const container = this.host.nativeElement;
+          this.containerCenter = container.clientWidth / 2;
+          const activeLink = container.querySelector('.selected') as HTMLElement;
+          if (activeLink) {
+            const offset = activeLink.offsetLeft + activeLink.offsetWidth / 2 - container.clientWidth / 2;
+            container.scrollTo({ left: offset, behavior: 'smooth' });
+          }
+        });
+      });
+    }
+  }
+  showTooltip(always = false): boolean {
+    const isMobile = window.innerWidth < SCREEN_SIZES.md;
+    return isMobile || (this.expanded() && always);
+  }
+
+  componentCount(): number {
+    return this.helpersService.enabledComponents().length;
+  }
+
 }
